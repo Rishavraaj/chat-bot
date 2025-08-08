308de8c4500c852e90d4a13f02357d1e4a908f87
Rishavraaj <rishavrajpatna7@gmail.com>
2025-08-08 21:08:06 +0530

    Add user data display in main page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Button } from "@workspace/ui/components/button";
 import { useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
@@ -10,6 +11,7 @@ export default function Page() {
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        <pre>{JSON.stringify(users, null, 2)}</pre>
       </div>
     </div>
   );
