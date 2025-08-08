8df4590f1ea5ef5bf4bb6c12e0fa3e90f25e1ff1
Rishavraaj <rishavrajpatna7@gmail.com>
2025-08-08 22:05:01 +0530

    Add UserButton component and create AuthLayout for authentication
    - Integrated UserButton from Clerk to display user information in the main page.
    - Created a new AuthLayout component to provide a consistent layout for authentication-related pages, enhancing user experience during sign-in and sign-up processes.

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -2,7 +2,7 @@
 import { Button } from "@workspace/ui/components/button";
 import { useQuery, Authenticated, Unauthenticated } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
-import { SignInButton } from "@clerk/nextjs";
+import { SignInButton, UserButton } from "@clerk/nextjs";
 
 export default function Page() {
   const users = useQuery(api.users.getUsers);
@@ -12,6 +12,7 @@ export default function Page() {
       <Authenticated>
         <div className="flex items-center justify-center min-h-svh">
           <div className="flex flex-col items-center justify-center gap-4">
+            <UserButton />
             <h1 className="text-2xl font-bold">Hello World</h1>
             <Button size="sm">Button</Button>
             <pre>{JSON.stringify(users, null, 2)}</pre>
