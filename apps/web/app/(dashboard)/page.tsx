1a2f598300bcd034274f6195150223e8ffbb535f
Rishavraaj <rishavrajpatna7@gmail.com>
2025-08-08 22:32:47 +0530

    Add OrganizationSwitcher to dashboard page for improved user experience
    - Integrated OrganizationSwitcher component from Clerk to allow users to switch between organizations seamlessly.
    - Enhanced the dashboard layout by including the OrganizationSwitcher alongside the UserButton, improving user navigation and accessibility.

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { Button } from "@workspace/ui/components/button";
 import { api } from "@workspace/backend/_generated/api";
-import { UserButton } from "@clerk/nextjs";
+import { UserButton, OrganizationSwitcher } from "@clerk/nextjs";
 import { useQuery } from "convex/react";
 
 export default function Page() {
@@ -12,6 +12,7 @@ export default function Page() {
       <div className="flex items-center justify-center min-h-svh">
         <div className="flex flex-col items-center justify-center gap-4">
           <UserButton />
+          <OrganizationSwitcher />
           <h1 className="text-2xl font-bold">Hello World</h1>
           <Button size="sm">Button</Button>
           <pre>{JSON.stringify(users, null, 2)}</pre>
